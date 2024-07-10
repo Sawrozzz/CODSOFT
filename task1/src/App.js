b0851a601c4c27584dfc09a79db152057c6ad767
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-07-10 21:09:17 +0545

    refactor

--- task1/src/App.js
+++ task1/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import About from "./components/About";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <div>
+      <React.Fragment>
         <Header />
-      </div>
-    </>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </React.Fragment>
   );
 };
 
--- task1/src/components/Home.js
+++ task1/src/components/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+    <img src="images.png" alt="myimage" className="home-image" />
+    <h1 className="home-title">Welcome to My Portfolio</h1>
+      <h2 className="home-subtitle">A showcase of my projects and skills</h2>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
