491d697dc1e6b7a77aa7b36810048fa9f62f476c
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-07-13 09:39:05 +0545

    refactor

--- task1/src/components/Contact.js
+++ task1/src/components/Contact.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <React.Fragment>
+      <section className="contact-main">
+       <form className="contact-form" method="post">
+       <h2>Contact Me</h2>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required></input>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required></input>
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+        <button type="submit">Submit</button>
+       </form>
+      </section>
+    </React.Fragment>
+  );
+};
 
-export default Contact
+export default Contact;
