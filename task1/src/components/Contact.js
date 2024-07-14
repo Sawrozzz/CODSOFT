09ec5f2b4312b4586ee16f6b3e44148551fbfea9
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-07-14 09:22:48 +0545

    added github link for projects & integrate IO form for contact

--- task1/src/components/Contact.js
+++ task1/src/components/Contact.js
@@ -4,16 +4,21 @@ const Contact = () => {
   return (
     <React.Fragment>
       <section className="contact-main">
-       <form className="contact-form" method="post">
-       <h2>Contact Me</h2>
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required></input>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required></input>
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-        <button type="submit">Submit</button>
-       </form>
+        <form
+          className="contact-form"
+          method="post"
+          encType="multipart/form-data"
+          action="https://getform.io/f/pamqmnoa"
+        >
+          <h2>Contact Me</h2>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required></input>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required></input>
+          <label for="message">Message:</label>
+          <textarea id="message" name="message" required></textarea>
+          <button type="submit">Submit</button>
+        </form>
       </section>
     </React.Fragment>
   );
--- task1/src/components/Projects.js
+++ task1/src/components/Projects.js
@@ -9,6 +9,7 @@ const Projects = () => {
       title: "Blog Website",
       description: "This is a blog website built with React and Node.js.",
       languages: ["React", "Node.js", "MongoDB"],
+      githubRepo: "https://github.com/Sawrozzz/Blog",
     },
     {
       image: "e-com.png",
@@ -16,31 +17,39 @@ const Projects = () => {
       description:
         "This is an E-commerce website built with Mongodb and Node.js.",
       languages: ["Node.js", "MongoDB"],
+      githubRepo: "https://github.com/Sawrozzz/Ecommerce_NODE",
     },
     {
       image: "tictactoe.png",
+
       title: "Tic Tac Toe Game",
-      description:
-        "This is a tic tac toe game that is build using react js.",
+      description: "This is a tic tac toe game that is build using react js.",
       languages: ["React.js", "CSS"],
+      githubRepo: "https://github.com/Sawrozzz/TicTacToe_React",
     },
     {
       image: "ui.png",
       title: "UI Design",
-      description:
-        "A simple UI design created using HTML and CSS.",
+      description: "A simple UI design created using HTML and CSS.",
       languages: ["Html", "css"],
+      githubRepo: "https://github.com/Sawrozzz/Login-Interface",
     },
   ];
   return (
     <div className="projects-container">
       {projectData.map((project, index) => (
         <section key={index} className="project-section">
-          <img
-            src={project.image}
-            alt={project.title}
-            className="project-image"
-          />
+          <a
+            href={project.githubRepo}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={project.image}
+              alt={project.title}
+              className="project-image"
+            />
+          </a>
           <h4>{project.title}</h4>
           <p>{project.description}</p>
           <div className="project-languages">
