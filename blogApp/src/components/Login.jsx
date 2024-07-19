8c95b7dc9513c6c59ff52e7eb8ad40c3c6d3f614
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-07-19 15:14:39 +0545

    change design

--- blogApp/src/components/Login.jsx
+++ blogApp/src/components/Login.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
 
 const Login = ({ setAuth }) => {
   const navigate = useNavigate();
@@ -16,11 +17,15 @@ const Login = ({ setAuth }) => {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
-        <h2 className=" text-4xl mb-6">Login</h2>
-        <p className="mb-4"> Please Login with google</p>
-        <button className="border border-black" onClick={signInWithGoogle}>
-          Login
+      <div className="flex flex-col justify-center items-center p-10 rounded-lg ">
+        <h1 className="text-6xl font-semibold mb-6 text-gray-700">Login Here</h1>
+        <p className="mb-4 text-2xl text-gray-500">Please Login with Google</p>
+        <button
+          onClick={signInWithGoogle}
+          className="flex items-center justify-center bg-white hover:bg-blue-700 text-gray-900 font-semibold py-2 px-4 border border-gray-300 rounded shadow"
+        >
+          <FaGoogle className="mr-2 text-red-500" />
+          Login with Google
         </button>
       </div>
     </>
