66085f0b62bc5576045f836c027c066c20096cbc
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-07-19 14:20:10 +0545

    added stuffs

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -8,37 +8,39 @@ import "./index.css";
 import { useState } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
-
+import UpdateBlog from "./components/UpdateBlog";
 
 const App = () => {
-  const [isAuth, setAuth] = useState(false);
+  const [isAuth, setAuth] = useState(localStorage.getItem("isAuth"));
   const Logout = () => {
-signOut(auth).then(()=>{
-  localStorage.clear();
-  setAuth(false);
-  window.location.assign("/login");
-});
-}
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setAuth(false);
+      window.location.assign("/login");
+    });
+  };
 
   return (
     <>
       <BrowserRouter>
         <nav className="bg-gray-900 text-2xl text-white flex justify-end gap-4 pr-10 py-5">
           <Link to="/">Home</Link>
-          <Link to="/create">Create Post</Link>
           {isAuth ? (
             <>
+              <Link to="/create">Create Post</Link>
+
               <Link to="/profile">Profile</Link>
-              <button  onClick={Logout}>Logout</button>
+              <button onClick={Logout}>Logout</button>
             </>
           ) : (
             <Link to="/login">Login</Link>
           )}
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home isAuth={isAuth} />} />
           <Route path="/login" element={<Login setAuth={setAuth} />} />
-          <Route path="/create" element={<CreateBlog />} />
+          <Route path="/create" element={<CreateBlog isAuth={isAuth} />} />
+          <Route path="/update/:id" element={<UpdateBlog isAuth={isAuth} />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
