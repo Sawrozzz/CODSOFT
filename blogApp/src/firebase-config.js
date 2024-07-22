fa36adc2dd2a73df58acf63edc4fa1b1f312dc7a
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-07-22 13:10:48 +0545

    configure new project setup

--- blogApp/src/firebase-config.js
+++ blogApp/src/firebase-config.js
@@ -1,17 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJXUebgc5OFP1nVvqA6F_nt1VrjBmF1nY",
-  authDomain: "blogapp-674a2.firebaseapp.com",
-  projectId: "blogapp-674a2",
-  storageBucket: "blogapp-674a2.appspot.com",
-  messagingSenderId: "282573272927",
-  appId: "1:282573272927:web:fa80345008fd2264ff68ea",
+  apiKey: "AIzaSyDyt6tinxsG8OOsiUWyEVreI7xvkdRUvhQ",
+
+  authDomain: "myblog-83056.firebaseapp.com",
+
+  projectId: "myblog-83056",
+
+  storageBucket: "myblog-83056.appspot.com",
+
+  messagingSenderId: "164267257189",
+
+  appId: "1:164267257189:web:07a91e32c63ee95ec4c58c",
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
